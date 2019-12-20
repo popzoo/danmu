102ee778a94f5a46985b4ffe7a9da014109c719e
lvlanxingapp <13653691+lvlanxingapp@users.noreply.github.com>
2019-12-20 22:36:15 +0800

    Add files via upload

--- js/danmu.js
+++ js/danmu.js
@@ -16,7 +16,39 @@ function fileImport() {
 	console.info("文件名:"+name+"; 大小:"+size);
 	// console.info(name.substr(0,name.length-5));
     if(parseInt(selectedFile.size)<1024*1024*5 && name.substr(name.length-5)==".json"){
-        bojiangCheck(1,name);
+    	// 这里对接播酱查验用户名查看用户真伪
+        fetch('https://bojianger.com/data/api/common/search.do?keyword='+name.substr(0,name.length-5), {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default',
+            credentials: 'omit'
+        }).then(result => {
+            return result.json();
+        }).then(json => {
+            let userName = json.data.audienceVo.audience_name;
+            let level = json.data.audienceVo.level;
+            console.info("用户名："+userName+";等级："+level)
+            if(userName == name.substr(0,name.length-5) && parseInt(level)>-1){
+            	// fileFlag = true;
+    			var reader = new FileReader();//这是核心,读取操作就是由它完成.
+    			reader.readAsText(selectedFile);//读取文件的内容,也可以读取文件的URL,默认编码utf-8
+    			reader.onload = function () {
+    			    //当读取完成后回调这个函数,然后此时文件的内容存储到了result中,直接操作即可
+    			    // console.log(this.result);
+    			    if(isJSON(this.result)){
+    			    	console.info("JSON格式校验正确，正在上传");
+    			    	putCOS(this.result,name);
+    			    }else{
+    			    	alert('JSON文件格式校验错误，请重新编辑JSON文件内容！');
+    			    }
+    			}
+            }else{
+                alert("用户名格式不正确，请重新输入");
+            }
+        }).catch(err => {
+        	alert("斗鱼用户名检测不存在，请将文本名命名为斗鱼昵称");
+            // console.error('REQUEST ERROR', err);
+        })              	
     }else{
     	alert("请上传不大于5MB且为JSON格式的文件");
     }
@@ -31,54 +63,17 @@ function isJSON(str) {
             }else{
                 return false;
             }
+
         } catch(e) {
             console.log('error：'+str+'!!!'+e);
             return false;
         }
     }
     return false;
-}
-// get Uid from Uname
-function bojiangCheck(code,name){
-    name = code===2 ? document.getElementById("seek_self_danmu").value.trim(): name;
-    fetch('https://bojianger.com/data/api/common/search.do?keyword='+name.substr(0,name.length-5), {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default',
-        credentials: 'omit'
-    }).then(result => {
-        return result.text();
-    }).then(txt => {
-        let json = JSON.parse(txt);
-        let userName = json.data.audienceVo.audience_name;
-        let uid = json.data.audienceVo.uid;
-        let level = json.data.audienceVo.level;
-        console.info("用户名："+userName+";uid："+uid+";等级："+level)
-        if(code===1){
-            if(userName == name.substr(0,name.length-5) && uid!=null){
-                var reader = new FileReader();//这是核心,读取操作就是由它完成.
-                reader.readAsText(selectedFile);//读取文件的内容,也可以读取文件的URL,默认编码utf-8
-                reader.onload = function () {//当读取完成后回调这个函数,然后此时文件的内容存储到了result中,直接操作即可
-                    if(isJSON(this.result)){// console.log(this.result);
-                        console.info("JSON格式校验正确，正在上传");
-                        putCOS(this.result,uid+'.json');
-                    }else{
-                        alert('JSON文件格式校验错误，请重新编辑JSON文件内容！');
-                    }
-                }
-            }else{
-                alert("用户名格式不正确，请重新输入");
-            }
-        }else if(code===2){
-            headCOS(uid)
-        }
-    }).catch(err => {
-        console.error('无法连接到服务器，请重试！');
-    })     
-}
+} 
 // ============================= New COS Operate =========================
 function putCOS(file,fileName){
-    var key = 'cloudDanmu/'+fileName;
+    var key = 'userDanmu/'+fileName;
     var fd = new FormData();
     fd.append('key', key);
     fd.append('Content-Type','');
@@ -103,8 +98,9 @@ function putCOS(file,fileName){
     })
 }
 //check auth
-function headCOS(uid){
-    let reqUrl =prefix+'cloudDanmu/'+uid+'.json';
+function headCOS(obj){
+    let nickName = document.getElementById("seek_self_danmu").value.trim();
+    let reqUrl =prefix+'userDanmu/'+nickName+'.json';
     fetch(reqUrl,{
         method: 'HEAD',
         mode: 'cors',
@@ -126,12 +122,11 @@ function headCOS(uid){
         alert("昵称验证失败");
     })
 }
-
 // get Danmu content
 function getCOS(){
     alert("网址受到许多次恶意攻击，为大家数据安全，暂时不支持下载，还请谅解！");
     // let nickName = document.getElementById("seek_self_danmu").value.trim();
-    // let reqUrl =prefix+'cloudDanmu/'+nickName+'.json';
+    // let reqUrl =prefix+'userDanmu/'+nickName+'.json';
     // fetch(reqUrl,{
     //     method: 'GET',
     //     mode: 'cors',
